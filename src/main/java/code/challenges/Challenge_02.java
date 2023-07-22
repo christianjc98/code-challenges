10974987004434b303fa8d01ee448a8d1bae06ff
christianjc7 <christian.janssen@onissolutions.com>
2023-07-21 19:30:02 -0600

    change in comment

--- src/main/java/code/challenges/Challenge_02.java
+++ src/main/java/code/challenges/Challenge_02.java
@@ -5,9 +5,9 @@ import java.util.*;
 public class Challenge_02 {
 
     /**
-     * For the solution I created a new hash map to store the nums in the array, them I iterated over the nums arr and in eahc iteration I first
+     * For the solution I created a new hash map to store the array values, them I iterated over the nums array and in each iteration I first
      * calculate the complement of nums[i] so the sum of both is equal to the target, after this I checked if the hash map contains the complement
-     * as a key and if so I returned a new array with the value of the key, that is the index, and the current index, if the map does not contain the
+     * as a key and if so I returned a new array with the value of the key which was the index, and the current index, if the map does not contain the
      * number as a key I just put the number as the key and the index as value.
      *
      * Time Complexity is O(n) because we only iterate over the array once
